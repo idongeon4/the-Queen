50e963a5c81013c6d20010692155989edf2986ad
idongeon4 <idongeon4@gmail.com>
2025-07-24 18:40:22 +0900

    Create checkAnswer2.js

---
+++ checkAnswer2.js
@@ -0,0 +1,9 @@
+exports.handler = async function(event, context) {
+    const { answer } = JSON.parse(event.body);
+    const correctAnswer = 'bdace';
+    const isCorrect = answer.toLowerCase() === correctAnswer;
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ correct: isCorrect })
+    };
+};
