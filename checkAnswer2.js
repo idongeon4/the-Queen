fc8d5cc444fca7802acbe647f5fad7091626ec5d
idongeon4 <idongeon4@gmail.com>
2025-07-24 18:42:57 +0900

    Delete checkAnswer2.js

--- checkAnswer2.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async function(event, context) {
-    const { answer } = JSON.parse(event.body);
-    const correctAnswer = 'bdace';
-    const isCorrect = answer.toLowerCase() === correctAnswer;
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ correct: isCorrect })
-    };
-};
